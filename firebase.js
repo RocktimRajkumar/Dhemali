3c638191e229ac3d87bebe923baba9dcae8ae79c
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-08-03 21:06:25 +0530

    init: Project strucutre created

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC2PJafY3sAkswiSMGTZA-7q3-sufjSxLI",
+    authDomain: "dhemali.firebaseapp.com",
+    databaseURL: "https://dhemali.firebaseio.com",
+    projectId: "dhemali",
+    storageBucket: "dhemali.appspot.com",
+    messagingSenderId: "966943974403",
+    appId: "1:966943974403:web:9b0ffa407d0f0006b71a36",
+    measurementId: "G-TG4NHTJNZ2"
+};
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Dhemali 🔥  app for sharing short-form video</h1>
+    </div>
+  );
+}
+
+export default App;
