02d2c33881ce2cda300c7981a2fcebd1f2da9a5d
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-08-03 22:16:55 +0530

    feat: component video added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Video from './Video'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Dhemali 🔥  app for sharing short-form video</h1>
+      <h1>Hello Dhemali <span role="img" aria-label="fire">🔥</span>  app for sharing short-form video</h1>
+      <div className="app_videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
