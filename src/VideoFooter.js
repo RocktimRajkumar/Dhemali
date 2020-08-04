cfbb87d64b9e6a21c38873928925ada742da4f8f
RocktimRajkumar <rajkumar.rocktim@gmail.com>
2020-08-04 21:58:06 +0530

    feat: Added dependency Material UI and ticker. Video Footer design done

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './VideoFooter.css'
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from 'react-ticker'
 
 export default function VideoFooter() {
     return (
@@ -7,6 +9,16 @@ export default function VideoFooter() {
             <div className="videoFooter_text">
                 <h4>@rocktim</h4>
                 <p>This is a description</p>
+                <div className="videoFooter_ticker">
+                    <MusicNoteIcon className="videoFooter_icon" />
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>Hello</p>
+                            </>
+                        )}
+                    </Ticker>
+                </div>
             </div>
             <img src="./music.png"
                 alt="spinTheRecord"
